78dac2945f0bcdf5aafdad67901c8b9186f070c9
s6704062616036-collab <s6704062616036@email.kmutnb.ac.th>
2025-12-15 16:42:46 +0700

    Update lab3_6.cpp

--- lab3_6.cpp
+++ lab3_6.cpp
@@ -1,21 +1,23 @@
-#include <stdio.h>
+##include <stdio.h>
 
 int main() {
     int score;
-    scanf("%d", &score);
-    if(score < 68) {
-        printf("D");
-    } else if(score >= 85) {
-        printf("A");
-    } else if(score >= 75) {
-        printf("B");
-    } else if(score >= 68) {
-        printf("C");
-    } else {
-        printf("F");
-    }
-    return 0;
+   	scanf("%d",&score);
+	if (score >= 68) {
+		if(score >= 85){
+			printf("A");
+		}else if(score >= 75){
+			printf("B");
+		}else if(score >= 68){
+			printf("C");
+		}
+	} else {
+	 	if(score >= 55){
+	 		printf("D");
+		 }else{
+		 	 printf("F");
+		 }			
+	}
 }
-
 
 
